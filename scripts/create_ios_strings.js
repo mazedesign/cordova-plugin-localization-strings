3054f84664e006212ea293cd2a9ff45c275d7ffd
b.maiorana <b.maiorana@playmoove.com>
2019-02-15 16:38:15 +0100

    urgh

--- scripts/create_ios_strings.js
+++ scripts/create_ios_strings.js
@@ -1,4 +1,4 @@
-import getTargetLang from 'shared';
+// import getTargetLang from 'shared';
 
 const fs = require('fs-extra');
 const _ = require('lodash');
@@ -165,3 +165,28 @@ module.exports = function (context) {
     return deferred.promise;
 };
 
+function getTargetLang(context) {
+    let targetLangArr = [];
+    const deferred = context.requireCordovaModule('q').defer();
+    const path = context.requireCordovaModule('path');
+    const glob = context.requireCordovaModule('glob');
+
+    glob("../res/lang/*/translations.json", function (err, langFiles) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            langFiles.forEach(function (langFile) {
+                const matches = langFile.match(/\/res\/lang\/(.*)\/translations.json/);
+                if (matches) {
+                    const langString = matches[1].split('_')[0]
+                    targetLangArr.push({
+                        lang: langString,
+                        path: path.join(context.opts.projectRoot, langFile)
+                    });
+                }
+            });
+            deferred.resolve(targetLangArr);
+        }
+    });
+    return deferred.promise;
+}
